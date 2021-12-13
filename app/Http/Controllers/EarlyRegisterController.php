3dc5acb2cd06840eaf29cfabe2cb480b985ec20c
DhioAnugrahPutra <dhio.ap@student.smkti.net>
2021-12-12 22:33:19 -0800

    uncomment validate

--- app/Http/Controllers/EarlyRegisterController.php
+++ app/Http/Controllers/EarlyRegisterController.php
@@ -18,29 +18,29 @@ class EarlyRegisterController extends Controller
     //store the data
     public function store(Request $request)
     {
-        //validate the request data first here
-        // $validator = Validator::make($request->all(),[
-        //     'nm_student' => 'required',
-        //     'sch_student' => 'required',
-        //     'mjr_student' => 'required',
-        //     'phn_student' => 'required|numeric|min:11|max:13',
-        //     'phn_parent' => 'required|numeric|min:11|max:13',
-        //     'addrs_student' => 'required',
-        // ],
-        // [
-        //     'nm_student.required' => 'Nama Siswa Wajib Diisi',
-        //     'sch_student.required' => 'Asal Sekolah Wajib Diisi',
-        //     'mjr_student.required' => 'Jurusan Wajib Dipilih',
-        //     'phn_student.required' => 'Nomor Handphone Wajib Diisi',
-        //     'phn_student.numeric' =>  'Nomor Harus Angka',
-        //     'phn_student.min' => "Nomor Minimal 11 Digit",
-        //     'phn_student.max' => "Nomor Maksimal 13 Digit",
-        //     'phn_parent.required' => 'Nomor Handphone Wajib Diisi',
-        //     'phn_parent.numeric' =>  'Nomor Harus Angka',
-        //     'phn_parent.min' => "Nomor Minimal 11 Digit",
-        //     'phn_parent.max' => "Nomor Maksimal 13 Digit",
-        //     'addrs_student.required' => "Alamat Lengkap Wajib Diisi",
-        // ]);
+        // validate the request data first here
+        $validator = Validator::make($request->all(),[
+            'nm_student' => 'required',
+            'sch_student' => 'required',
+            'mjr_student' => 'required',
+            'phn_student' => 'required|numeric|min:11|max:13',
+            'phn_parent' => 'required|numeric|min:11|max:13',
+            'addrs_student' => 'required',
+        ],
+        [
+            'nm_student.required' => 'Nama Siswa Wajib Diisi',
+            'sch_student.required' => 'Asal Sekolah Wajib Diisi',
+            'mjr_student.required' => 'Jurusan Wajib Dipilih',
+            'phn_student.required' => 'Nomor Handphone Wajib Diisi',
+            'phn_student.numeric' =>  'Nomor Harus Angka',
+            'phn_student.min' => "Nomor Minimal 11 Digit",
+            'phn_student.max' => "Nomor Maksimal 13 Digit",
+            'phn_parent.required' => 'Nomor Handphone Wajib Diisi',
+            'phn_parent.numeric' =>  'Nomor Harus Angka',
+            'phn_parent.min' => "Nomor Minimal 11 Digit",
+            'phn_parent.max' => "Nomor Maksimal 13 Digit",
+            'addrs_student.required' => "Alamat Lengkap Wajib Diisi",
+        ]);
 
         EarlyRegister::create($request->all());
         return redirect()->route('index.pendaftar')->with('success', 'Data Berhasil Di Tambahkan');
